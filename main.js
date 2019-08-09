be07035bf3204ce15e7fb012d3c273e358fc0d03
Bruemor <arymer1@gmail.com>
2019-08-09 19:25:11 -0400

    Intro to javascript Carolina Code School Foundations

---
+++ main.js
@@ -0,0 +1,30 @@
+
+
+var firstNumber = 4;
+
+var secondNumber = 2;
+
+firstNumber = 10;
+
+var sum = firstNumber + secondNumber;
+
+var thing;
+
+var sentence = 'you\'re';
+
+var person = {
+	name: "Andrew",
+	age: 29,
+	favoriteColor: "Green"
+}
+
+var car = "my broken car ";
+var yesterday = "yesterday";
+var carVerb = "broke down ";
+// Way 1
+var phrase1 = "I got dinner " + yesterday;
+console.log(phrase1);
+// Way 2
+console.log("Way 2");
+
+console.log( car + carVerb + yesterday );
